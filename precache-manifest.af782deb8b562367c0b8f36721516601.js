5f3868b1ae699371169430b62f7c0a06fca6111c
Wojtek <Wojtek@DESKTOP-4FI9M6M>
2023-09-04 23:32:18 +0200

    Updates

---
+++ precache-manifest.af782deb8b562367c0b8f36721516601.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab102a7af55336b87f68",
+    "url": "/tic_tac_toe/static/css/main.ebae11f9.chunk.css"
+  },
+  {
+    "revision": "ab102a7af55336b87f68",
+    "url": "/tic_tac_toe/static/js/main.c7451ed2.chunk.js"
+  },
+  {
+    "revision": "9893aaba8da85bc22bd3",
+    "url": "/tic_tac_toe/static/js/runtime~main.17f91065.js"
+  },
+  {
+    "revision": "4f8549dffa947a35abb9",
+    "url": "/tic_tac_toe/static/js/2.b1c4f61e.chunk.js"
+  },
+  {
+    "revision": "cecf616dbfc830b280dc",
+    "url": "/tic_tac_toe/static/js/3.d7f7bd74.chunk.js"
+  },
+  {
+    "revision": "035bb82cf3cdf423aee83f826ee4c620",
+    "url": "/tic_tac_toe/index.html"
+  }
+];
